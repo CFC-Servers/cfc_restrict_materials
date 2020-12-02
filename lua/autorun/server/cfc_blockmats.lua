e7e317ba413dde30b997c77f419b7a3c8e867208
Stoob <steejerbs@gmail.com>
2020-12-02 13:26:26 -0600

    Added check for nil material

--- lua/autorun/server/cfc_blockmats.lua
+++ lua/autorun/server/cfc_blockmats.lua
@@ -4,13 +4,13 @@ local SubMat = ENT.SetSubMaterial
 local sub    = string.sub
 
 function ENT:SetMaterial(Mat)
-    if sub(Mat, 1, 3) == "pp/" then return end -- Reject setting any material that starts with "pp/"
+    if Mat and sub(Mat, 1, 3) == "pp/" then return end -- Reject setting any material that starts with "pp/"
 
     SetMat(self, Mat)
 end
 
 function ENT:SetSubMaterial(Idx, Mat)
-    if sub(Mat, 1, 3) == "pp/" then return end
+    if Mat and sub(Mat, 1, 3) == "pp/" then return end
 
     SubMat(self, Idx, Mat)
 end
