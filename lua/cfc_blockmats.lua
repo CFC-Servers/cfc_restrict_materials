e5f35fabf76662abf72b8b6c46501f496f64577d
Redox <69946827+wrefgtzweve@users.noreply.github.com>
2021-10-29 03:05:14 +0200

    Move file to shared

---
+++ lua/cfc_blockmats.lua
@@ -0,0 +1,16 @@
+local ENT    = FindMetaTable("Entity")
+local SetMat = ENT.SetMaterial
+local SubMat = ENT.SetSubMaterial
+local sub    = string.sub
+
+function ENT:SetMaterial(Mat)
+    if Mat and sub(Mat, 1, 3) == "pp/" then return end -- Reject setting any material that starts with "pp/"
+
+    SetMat(self, Mat)
+end
+
+function ENT:SetSubMaterial(Idx, Mat)
+    if Mat and sub(Mat, 1, 3) == "pp/" then return end
+
+    SubMat(self, Idx, Mat)
+end
