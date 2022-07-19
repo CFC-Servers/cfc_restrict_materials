4dee94e5f142c362cf08e06fbb3976a0444cd7f2
Brandon Sturgeon <brandon@brandonsturgeon.com>
2022-07-18 19:30:56 -0700

    oops

--- lua/autorun/cfc_blockmats.lua
+++ lua/autorun/cfc_blockmats.lua
@@ -2,5 +2,5 @@ AddCSLuaFile( "cfc_blockmat/modules/materials.lua" )
 include( "cfc_blockmat/modules/materials.lua")
 
 if SERVER then
-    include( "cfc_blockmats.lua" )
+    include( "cfc_blockmat/sv_init.lua" )
 end
