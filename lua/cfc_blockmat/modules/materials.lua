327150da57b8eb289c68b8c2a516f33718caa570
Brandon Sturgeon <brandon@brandonsturgeon.com>
2023-01-12 13:27:45 -0800

    Fix linting violations

--- lua/cfc_blockmat/modules/materials.lua
+++ lua/cfc_blockmat/modules/materials.lua
@@ -1,12 +1,12 @@
 local string_sub = string.sub
 
-local ENT = FindMetaTable("Entity")
+local ENT = FindMetaTable( "Entity" )
 ENT._SetMaterial = ENT._SetMaterial or ENT.SetMaterial
 ENT._SetSubMaterial = ENT._SetSubMaterial or ENT.SetSubMaterial
 
 
 function ENT:SetMaterial( mat )
-    if mat and string_sub(mat, 1, 3) == "pp/" then return end
+    if mat and string_sub( mat, 1, 3 ) == "pp/" then return end
 
     ENT._SetMaterial( self, mat )
 end
